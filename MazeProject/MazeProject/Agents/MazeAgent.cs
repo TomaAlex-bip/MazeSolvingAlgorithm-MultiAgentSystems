5cf8966c2d36e10befe116cd3f25f1b6c811f39b
Alexandru Toma <toma.alex.xd@gmail.com>
2024-12-05 17:46:43 +0200

    added basic logic for the agent

--- MazeProject/MazeProject/Agents/MazeAgent.cs
+++ MazeProject/MazeProject/Agents/MazeAgent.cs
@@ -15,9 +15,25 @@ namespace MazeProject.Agents
         public int Y { get; private set; }
         public int OldX { get; private set; }
         public int OldY { get; private set; }
+        public MoveDirection? ComingDirection
+        {
+            get
+            {
+                if (X > OldX)
+                    return MoveDirection.Left;
+                if (X < OldX)
+                    return MoveDirection.Right;
+                if (Y > OldY)
+                    return MoveDirection.Up;
+                if (Y < OldY)
+                    return MoveDirection.Down;
+                return MoveDirection.None;
+            }
+        }
 
         private readonly Maze _maze;
         private readonly Random _random;
+        private bool _isInDeadEnd = false;
 
         public MazeAgent(Maze maze, string name)
         {
@@ -45,7 +61,93 @@ namespace MazeProject.Agents
 
         public void MakeTurn()
         {
-            MoveRandomly();
+            var status = VerifyIfFinishIsReachable();
+            if (status)
+                return;
+
+            Move();
+
+            var w = -0.1f;
+            if (_isInDeadEnd)
+                w = -1f;
+            MoveData moveData = new(X, Y, OldX, OldY, w);
+            Broadcast(moveData, true);
+            OnAgentMoveEvent?.Invoke(moveData);
+        }
+
+        private void Move()
+        {
+            List<Tuple<MoveDirection, float>> moveDirections = new();
+            float maxWeight = 0f;
+            if (CanMoveUp())
+            {
+                var w = (float)_maze.Cells[X, Y].UpWeight!;
+                moveDirections.Add(new(MoveDirection.Up, w));
+                if (w > maxWeight)
+                    maxWeight = w;
+            }
+            if (CanMoveDown())
+            {
+                var w = (float)_maze.Cells[X, Y].DownWeight!;
+                moveDirections.Add(new(MoveDirection.Down, w));
+                if (w > maxWeight)
+                    maxWeight = w;
+            }
+            if (CanMoveLeft())
+            {
+                var w = (float)_maze.Cells[X, Y].LeftWeight!;
+                moveDirections.Add(new(MoveDirection.Left, w));
+                if (w > maxWeight)
+                    maxWeight = w;
+            }
+            if (CanMoveRight())
+            {
+                var w = (float)_maze.Cells[X, Y].RightWeight!;
+                moveDirections.Add(new(MoveDirection.Right, w));
+                if (w > maxWeight)
+                    maxWeight = w;
+            }
+
+            var validMoveDirections = moveDirections
+                .Where(x => x.Item2 >= maxWeight)
+                .Where(x => x.Item1 != ComingDirection)
+                .ToList();
+
+            var validMoveDirections2 = moveDirections
+                .Where(x => x.Item1 != ComingDirection)
+                .ToList();
+
+            if (validMoveDirections2.Count > 1)
+            {
+                _isInDeadEnd = false;
+            }
+
+            if (validMoveDirections.Count == 0)
+            {
+                validMoveDirections.Add(new((MoveDirection)ComingDirection!, 0));
+                _isInDeadEnd = true;
+            }
+
+            var direction = validMoveDirections[_random.Next(validMoveDirections.Count)];
+            
+            switch (direction.Item1)
+            {
+                case MoveDirection.Up:
+                    MoveUp();
+                    break;
+                case MoveDirection.Down:
+                    MoveDown();
+                    break;
+                case MoveDirection.Left:
+                    MoveLeft();
+                    break;
+                case MoveDirection.Right:
+                    MoveRight();
+                    break;
+                default:
+                    MoveRandomly();
+                    break;
+            }
         }
 
         private void MoveRandomly()
@@ -77,10 +179,6 @@ namespace MazeProject.Agents
                         break;
                 }
             }
-
-            MoveData moveData = new(X, Y, OldX, OldY, -0.1f);
-            Broadcast(moveData);
-            OnAgentMoveEvent?.Invoke(moveData);
         }
 
         private bool VerifyIfFinishIsReachable()
@@ -114,10 +212,7 @@ namespace MazeProject.Agents
 
         private bool MoveUp()
         {
-            if (Y - 1 < 0)
-                return false;
-
-            if (_maze.Cells[X, Y - 1].CellType != (int)MazeCell.Path)
+            if (!CanMoveUp())
                 return false;
 
             OldX = X;
@@ -128,9 +223,7 @@ namespace MazeProject.Agents
 
         private bool MoveDown()
         {
-            if (Y + 1 >= _maze.Cells.GetLength(0))
-                return false;
-            if (_maze.Cells[X, Y + 1].CellType != (int)MazeCell.Path)
+            if (!CanMoveDown())
                 return false;
 
             OldX = X;
@@ -141,9 +234,7 @@ namespace MazeProject.Agents
 
         private bool MoveRight()
         {
-            if (X + 1 >= _maze.Cells.GetLength(1))
-                return false;
-            if (_maze.Cells[X + 1, Y].CellType != (int)MazeCell.Path)
+            if (!CanMoveRight())
                 return false;
 
             OldX = X;
@@ -154,9 +245,7 @@ namespace MazeProject.Agents
 
         private bool MoveLeft()
         {
-            if (X - 1 < 0)
-                return false;
-            if (_maze.Cells[X - 1, Y].CellType != (int)MazeCell.Path)
+            if (!CanMoveLeft())
                 return false;
 
             OldX = X;
@@ -165,6 +254,45 @@ namespace MazeProject.Agents
             return true;
         }
 
-        
+        private bool CanMoveUp()
+        {
+            if (Y - 1 < 0)
+                return false;
+            if (_maze.Cells[X, Y - 1].CellType != (int)MazeCell.Path)
+                return false;
+            return true;
+        }
+
+        private bool CanMoveDown()
+        {
+            if (Y + 1 >= _maze.Cells.GetLength(0))
+                return false;
+            if (_maze.Cells[X, Y + 1].CellType != (int)MazeCell.Path)
+                return false;
+            return true;
+        }
+
+        private bool CanMoveRight()
+        {
+            if (X + 1 >= _maze.Cells.GetLength(1))
+                return false;
+            if (_maze.Cells[X + 1, Y].CellType != (int)MazeCell.Path)
+                return false;
+            return true;
+        }
+
+        private bool CanMoveLeft()
+        {
+            if (X - 1 < 0)
+                return false;
+            if (_maze.Cells[X - 1, Y].CellType != (int)MazeCell.Path)
+                return false;
+            return true;
+        }
+
+        public enum MoveDirection
+        {
+            Up, Down, Left, Right, None
+        }
     }
 }
